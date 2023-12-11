e00761fa5eb06859edb6fc31dea1823c48b981ab
Into-The-Grey <nicholas.acord@mailfence.com>
2023-12-10 22:10:11 -0500

    Add audio metadata extraction using Mutagen library

--- MindMatrix/DataOrganizerAI/Scripts/FileParsing/audio_metadata_extractor.py
+++ MindMatrix/DataOrganizerAI/Scripts/FileParsing/audio_metadata_extractor.py
@@ -1,48 +1,66 @@
 import os
-import logging
-from pydub.utils import mediainfo
-import json
-import time
+from mutagen.easyid3 import EasyID3
+from mutagen.flac import FLAC
+from mutagen.mp3 import MP3
+from mutagen.wavpack import WavPack
 from tqdm import tqdm
+import humanize
+from mutagen._util import MutagenError
+# ... other necessary imports
 
-def extract_audio_metadata(audio_path, output_dir):
+def extract_metadata(file_path):
+    """
+    Extracts metadata from an audio file.
+    Supports various formats like mp3, flac, wavpack, etc.
+    """
+    metadata = {}
     try:
-        metadata = mediainfo(audio_path)
-        output_file_path = os.path.join(output_dir, os.path.basename(audio_path) + '.json')
-        with open(output_file_path, 'w') as file:
-            json.dump(metadata, file)
-        logging.info("Metadata extracted for: %s", audio_path)
-        return True
-    except Exception as e:
-        logging.error("Failed to extract metadata for: %s. Error: %s", audio_path, str(e))
-        return False
+        audio = None  # Add type hint to avoid "possibly unbound" error
+        if file_path.endswith('.mp3'):
+            audio = MP3(file_path, ID3=EasyID3)
+        elif file_path.endswith('.flac'):
+            audio = FLAC(file_path)
+        elif file_path.endswith('.wv'):
+            audio = WavPack(file_path)
+        # ... include other formats as necessary
+
+        if audio is not None:
+            for key in audio.keys():
+                metadata[key] = audio[key]
+
+    except (MutagenError, IOError) as e:
+        print(f"Error processing {file_path}: {e}")
+        return None
+
+    return metadata
+
+def save_metadata(metadata, save_path):
+    """
+    Saves the extracted metadata to a file.
+    """
+    # Implement logic to save metadata in a structured format (e.g., JSON)
+
+def process_audio_files(audio_dir, metadata_dir):
+    audio_files = [f for f in os.listdir(audio_dir) if f.endswith(('.mp3', '.flac', '.wv'))] # Extend for other formats
+
+    with tqdm(total=len(audio_files), unit='file', desc="Processing Audio Files") as pbar:
+        for file in audio_files:
+            file_path = os.path.join(audio_dir, file)
+            file_size = os.path.getsize(file_path)
+            print(f"Processing {file} (Size: {humanize.naturalsize(file_size)})")
+
+            metadata = extract_metadata(file_path)
+            if metadata:
+                save_path = os.path.join(metadata_dir, os.path.basename(file) + '.json')
+                save_metadata(metadata, save_path)
+            
+            pbar.update(1)
+            pbar.set_postfix_str(f"{pbar.n}/{pbar.total} files processed")
 
 def main():
-    logging.basicConfig(filename='audio_metadata_extraction.log', level=logging.INFO)
-    sorted_audio_dir = '/home/ncacord/Desktop/raphael_core/AI_Core/DataOrganizerAI/SortedFiles/Audio'  # Update this path
-    metadata_output_dir = '/home/ncacord/Desktop/raphael_core/AI_Core/DataOrganizerAI/ParsedData/AudioMetadata'  # Update this path
+    audio_dir = 'path/to/audio/files'
+    metadata_dir = 'path/to/ParsedData/AudioMetadata'
+    process_audio_files(audio_dir, metadata_dir)
 
-    if not os.path.exists(metadata_output_dir):
-        os.makedirs(metadata_output_dir)
-
-    audio_files = [filename for filename in os.listdir(sorted_audio_dir) if filename.lower().endswith(('.mp3', '.wav', '.aac', '.flac'))]
-    total_files = len(audio_files)
-
-    print("Audio metadata extraction started.")
-    start_time = time.time()
-
-    with tqdm(total=total_files) as pbar:
-        for filename in audio_files:
-            file_path = os.path.join(sorted_audio_dir, filename)
-
-            if extract_audio_metadata(file_path, metadata_output_dir):
-                pbar.set_postfix({"Processing file": filename})
-                pbar.update(1)
-            else:
-                pbar.set_postfix({"Failed file": filename})
-                pbar.update(1)
-
-    print("Audio metadata extraction complete.")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
