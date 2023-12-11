32f7752c7d682ac243db154502277e81336eef1c
Into-The-Grey <nicholas.acord@mailfence.com>
2023-12-11 02:01:44 -0500

    Refactor file sorting script and improve error handling

--- MindMatrix/DataOrganizerAI/Scripts/FileSorting/sort_by_type.py
+++ MindMatrix/DataOrganizerAI/Scripts/FileSorting/sort_by_type.py
@@ -1,62 +1,100 @@
+"""_summary_Returns:_type_: _description_"""
 import os
 import shutil
+import logging
 import zipfile
 import tarfile
 import rarfile  # Requires 'rarfile' package
-import logging
 
 # Set up logging
-logging.basicConfig(filename='MindMatrix/DataOrganizerAI/Logs/CategorizationLogs/file_sorting.log',
-                    level=logging.ERROR,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
+logging.basicConfig(
+    filename="MindMatrix/DataOrganizerAI/Logs/CategorizationLogs/file_sorting.log",
+    level=logging.ERROR,
+    format="%(asctime)s:%(levelname)s:%(message)s",
+)
 
 # Directories
-source_dir = 'DataDumps'
-sorted_dir = 'MindMatrix/DataOrganizerAI/SortedFiles'
-uncompressible_dir = 'MindMatrix/DataOrganizerAI/Uncompressible'
+UNCOMPRESSIBLE_DIR = "MindMatrix/DataOrganizerAI/Uncompressible"
+SORTED_DIR = "MindMatrix/DataOrganizerAI/Sorted"  # Define the SORTED_DIR variable
 
 # Ensure existence of necessary directories
-os.makedirs(sorted_dir, exist_ok=True)
-os.makedirs(uncompressible_dir, exist_ok=True)
+os.makedirs(SORTED_DIR, exist_ok=True)
+os.makedirs(UNCOMPRESSIBLE_DIR, exist_ok=True)
 
 # Supported file types for sorting
 file_types = {
-    '.txt': 'Documents',
-    '.pdf': 'PDFs',
-    '.jpg': 'Images',
-    '.png': 'Images',
-    '.mp3': 'Audio',
-    '.wav': 'Audio',
-    '.mp4': 'Videos',
-    '.avi': 'Videos'
+    ".txt": "Documents",
+    ".pdf": "PDFs",
+    ".jpg": "Images",
+    ".png": "Images",
+    ".mp3": "Audio",
+    ".wav": "Audio",
+    ".mp4": "Videos",
+    ".avi": "Videos"
     # Add other file types as needed
 }
 
 # Supported archive formats
-archive_formats = ['.zip', '.tar', '.rar']
+archive_formats = [".zip", ".tar", ".rar"]
+
 
 def uncompress_file(file_path, destination):
+    """
+    Uncompresses a file to the specified destination.
+
+    Args:
+        file_path (str): The path of the file to be uncompressed.
+        destination (str): The destination directory where the uncompressed files will be placed.
+
+    Returns:
+        bool: True if the file was successfully uncompressed, False otherwise.
+    """
     try:
-        if file_path.endswith('.zip'):
-            with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        if file_path.endswith(".zip"):
+            with zipfile.ZipFile(file_path, "r") as zip_ref:
                 zip_ref.extractall(destination)
-        elif file_path.endswith('.tar'):
-            with tarfile.open(file_path, 'r') as tar_ref:
+        elif file_path.endswith(".tar"):
+            with tarfile.open(file_path, "r") as tar_ref:
                 tar_ref.extractall(destination)
-        elif file_path.endswith('.rar'):
-            with rarfile.RarFile(file_path, 'r') as rar_ref:
+        elif file_path.endswith(".rar"):
+            with rarfile.RarFile(file_path, "r") as rar_ref:
                 rar_ref.extractall(destination)
         return True
-    except Exception as e:
-        logging.error(f"Failed to uncompress {file_path}: {e}")
+    except zipfile.BadZipFile as e:
+        logging.error("Failed to uncompress %s: %s", file_path, e)
+        return False
+    except tarfile.TarError as e:
+        logging.error("Failed to uncompress %s: %s", file_path, e)
+        return False
+    except rarfile.RarCannotExec as e:
+        logging.error("Failed to uncompress %s: %s", file_path, e)
         return False
 
+
 def sort_files():
+    """
+    Sorts files in a directory based on their file extension.
+
+    This function iterates through all the files in the source directory and
+    categorizes them based on their file extension. Files with recognized
+    extensions are moved to their respective destination directories, while
+    unrecognized file types are moved to a separate directory.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    source_dir = "MindMatrix/DataOrganizerAI/Source"  # Define the source_dir variable
+
     for file in os.listdir(source_dir):
         file_path = os.path.join(source_dir, file)
         file_extension = os.path.splitext(file)[1].lower()
 
         # Check if file is an archive and uncompress it
+        uncompressible_dir = "MindMatrix/DataOrganizerAI/Uncompressible"  # Define the uncompressible_dir variable
+
         if file_extension in archive_formats:
             if uncompress_file(file_path, source_dir):
                 os.remove(file_path)  # Remove the archive after extraction
@@ -66,12 +104,15 @@ def sort_files():
 
         # Sort files into categories
         if file_extension in file_types:
-            destination_dir = os.path.join(sorted_dir, file_types[file_extension])
+            destination_dir = os.path.join(
+                SORTED_DIR, file_types[file_extension]
+            )  # Replace 'sorted_dir' with 'SORTED_DIR'
             os.makedirs(destination_dir, exist_ok=True)
             shutil.move(file_path, destination_dir)
         else:
-            # Move unrecognised file types to uncompressible
+            # Move unrecognized file types to uncompressible
             shutil.move(file_path, uncompressible_dir)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sort_files()
