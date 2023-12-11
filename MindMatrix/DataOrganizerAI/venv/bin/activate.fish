84b5addab5dbe603bdca2981c74a821dc2771696
Into-The-Grey <nicholas.acord@mailfence.com>
2023-12-10 23:49:35 -0500

    Add new files and directories

--- MindMatrix/DataOrganizerAI/venv/bin/activate.fish
+++ MindMatrix/DataOrganizerAI/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ncacord/Desktop/DataOrganizerAI/venv"
+set -gx VIRTUAL_ENV "/home/ncacord/Desktop/Raphael/MindMatrix/DataOrganizerAI/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
